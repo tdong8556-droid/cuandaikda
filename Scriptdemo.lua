6bbc64c9d9ff9b4b3d90601db2f276b61aae0e12
tdong8556-droid <tdong8556@gmail.com>
2025-08-27 19:01:28 +0700

    Update Scriptdemo.lua

--- Scriptdemo.lua
+++ Scriptdemo.lua
@@ -1,24 +1,50 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
 
-       local Window = MakeWindow({
-         Hub = {
-         Title = "cuandaika!@",
-         Animation = "Youtube: TBoy Roblox"
-         },
-        Key = {
-        KeySystem = false,
-        Title = "Key System",
-        Description = "",
-        KeyLink = "",
-        Keys = {"1234"},
-        Notifi = {
-        Notifications = true,
-        CorrectKey = "Running the Script...",
-       Incorrectkey = "The key is incorrect",
-       CopyKeyLink = "Copied to Clipboard"
-      }
+       function love.load()
+    font = love.graphics.newFont(18)
+    love.graphics.setFont(font)
+
+    button = {
+        x = 200,
+        y = 200,
+        w = 200,
+        h = 60,
+        text = "Bấm vào tôi!",
+        hovered = false
     }
-  })
+end
+
+function love.update(dt)
+    local mx, my = love.mouse.getPosition()
+    button.hovered = mx > button.x and mx < button.x + button.w and my > button.y and my < button.y + button.h
+end
+
+function love.draw()
+    -- nền
+    love.graphics.clear(30/255, 30/255, 30/255)
+
+    -- nút
+    if button.hovered then
+        love.graphics.setColor(0.2, 0.6, 1) -- xanh sáng khi hover
+    else
+        love.graphics.setColor(0.1, 0.4, 0.8) -- xanh đậm
+    end
+    love.graphics.rectangle("fill", button.x, button.y, button.w, button.h, 12, 12)
+
+    -- viền
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setLineWidth(3)
+    love.graphics.rectangle("line", button.x, button.y, button.w, button.h, 12, 12)
+
+    -- chữ
+    love.graphics.printf(button.text, button.x, button.y + 18, button.w, "center")
+end
+
+function love.mousepressed(x, y, buttonNum)
+    if buttonNum == 1 and button.hovered then
+        button.text = "Đã bấm!"
+    end
+end
 
        MinimizeButton({
        Image = "http://www.roblox.com/asset/?id=83190276951914",
