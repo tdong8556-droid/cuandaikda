f812ac51032dc056f3aa27f2e955f275c19c0d9e
tdong8556-droid <tdong8556@gmail.com>
2025-08-27 19:23:02 +0700

    Update Scriptdemo.lua

--- Scriptdemo.lua
+++ Scriptdemo.lua
@@ -1,50 +1,32 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/tdong8556-droid/cuandaikda/refs/heads/main/Scriptdemo.lua")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
 
-       lfunction love.load()
-    font = love.graphics.newFont(18)
-    love.graphics.setFont(font)
-
-    button = {
-        x = 200,
-        y = 200,
-        w = 200,
-        h = 60,
-        text = "tapme!",
-        hovered = false
+       local Window = MakeWindow({
+         Hub = {
+         Title = "cuandaika",
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
     }
-end
+  })
 
-function love.update(dt)
-    local mx, my = love.mouse.getPosition()
-    button.hovered = mx > button.x and mx < button.x + button.w and my > button.y and my < button.y + button.h
-end
-
-function love.draw()
-    -- nền
-    love.graphics.clear(30/255, 30/255, 30/255)
-
-    -- nút
-    if button.hovered then
-        love.graphics.setColor(0.2, 0.6, 1) -- xanh sáng khi hover
-    else
-        love.graphics.setColor(0.1, 0.4, 0.8) -- xanh đậm
-    end
-    love.graphics.rectangle("fill", button.x, button.y, button.w, button.h, 12, 12)
-
-    -- viền
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.setLineWidth(3)
-    love.graphics.rectangle("line", button.x, button.y, button.w, button.h, 12, 12)
-
-    -- chữ
-    love.graphics.printf(button.text, button.x, button.y + 18, button.w, "center")
-end
-
-function love.mousepressed(x, y, buttonNum)
-    if buttonNum == 1 and button.hovered then
-        button.text = "Đã bấm!"
-    end
-end
+       MinimizeButton({
+       Image = "https://cdn.discordapp.com/attachments/974606838664417320/1410237969054568488/IMG_2432.jpg?ex=68b049f4&is=68aef874&hm=55081f7e3a1ead4efb6c7bfea31da80af8ed7ee211a5e70a57586a580891486b&",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
       
 ------ Tab
      local Tab1o = MakeTab({Name = "Script Farm"})
@@ -52,7 +34,7 @@ end
 ------- BUTTON
     
     AddButton(Tab1o, {
-     Name = "cuan Hub",
+     Name = "Redz Hub",
     Callback = function()
 	  local Settings = {
   JoinTeam = "Pirates"; -- Pirates/Marines
